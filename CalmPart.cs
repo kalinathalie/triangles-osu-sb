074c7807b8e10f754654b73f399900d28306bef2
Kali Nathalie <kalinathalie@outlook.com>
2022-07-21 01:08:43 -0300

    finish calmpart

--- CalmPart.cs
+++ CalmPart.cs
@@ -61,8 +61,9 @@ namespace StorybrewScripts
             var triangleCentre = new Vector2(320, 240);
 
             int run = 0;
-            for(double tempo = StartTime-tick(0,(double)1/(double)16); tempo < EndTime; tempo+=tick(0,6)){
-
+            for(double tempo = StartTime-tick(0,(double)1/(double)16); tempo < 73136; tempo+=tick(0,6)){
+                
+                if((tempo>=69761 && tempo<=71261) || (tempo>=63761 && tempo<=64886)) continue;
                 var color2 = Color;
                 if (ColorVariance > 0){
                     ColorVariance = MathHelper.Clamp(ColorVariance, 0, 1);
@@ -86,9 +87,10 @@ namespace StorybrewScripts
                     triangle2 = layer.CreateSprite(Triangle2, OsbOrigin.Centre);
                 }
                 
+                var randomScale = Random(0.1,0.25);
                 if (color2.R != 1 || color2.G != 1 || color2.B != 1)
                     triangle2.Color(StartTime, color2);
-                    triangle2.Scale(StartTime, Random(0.1,0.25));
+                    triangle2.Scale(StartTime, randomScale);
 
                 var RandomBorder2 = Random(1,5);
                 var newPositiontriangle2 = new Vector2(320,240);
@@ -108,15 +110,22 @@ namespace StorybrewScripts
                 }
                 
                 var RandomFade2 = Random(0.6, 1);
-                triangle2.Move(tempo, Random(tempo+tick(0,(double)1/(double)15),tempo+tick(0,(double)1/(double)32)), triangleCentre, newPositiontriangle2);
+                var RandomEndTime = Random(tempo+tick(0,(double)1/(double)15),tempo+tick(0,(double)1/(double)32));
+                
+                triangle2.Move(tempo, RandomEndTime, triangleCentre, newPositiontriangle2);
                 triangle2.Fade(StartTime-tick(0,(double)1/(double)16), 0);
                 if(tempo<StartTime){
                     triangle2.Fade(StartTime, EndTime, RandomFade2, RandomFade2);
                 }else{
                     triangle2.Fade(tempo, EndTime, RandomFade2, RandomFade2);
                 }
+
+                if(RandomEndTime >= 73886){
+                    triangle2.Scale((OsbEasing)4, 73886, 74261, randomScale, randomScale*1.5);
+                    triangle2.Scale((OsbEasing)4, 74261, 75011, randomScale*1.5, 0);
+                }
                 
-                triangle2.Rotate(tempo, Random(tempo+tick(0,(double)1/(double)15),tempo+tick(0,(double)1/(double)32)), MathHelper.DegreesToRadians(Random(-150,150)), MathHelper.DegreesToRadians(Random(-150,150)));
+                triangle2.Rotate(tempo, RandomEndTime, MathHelper.DegreesToRadians(Random(-150,150)), MathHelper.DegreesToRadians(Random(-150,150)));
                 triangle2.Fade(EndTime, 0);
                 run+=1;
             }
@@ -124,8 +133,54 @@ namespace StorybrewScripts
             var brilho = layer.CreateSprite(Ponto, OsbOrigin.Centre);
             brilho.Scale(StartTime, 30);
             brilho.Fade(StartTime, 1);
-            brilho.Fade(EndTime, EndTime + 500, 1, 0);
+            brilho.Fade((OsbEasing)7, 73886, 74636, 1, 0);
             brilho.Color(StartTime, new Color4(255,130,180,255));
+
+            var triangulao = layer.CreateSprite(Triangle1, OsbOrigin.Centre, new Vector2(320, 240));
+            triangulao.Fade(StartTime, 1);
+            triangulao.Fade(EndTime, EndTime + 500, 1, 0);
+            triangulao.Color(StartTime, new Color4(200, 20, 120, 255));
+            
+            triangulao.Rotate((OsbEasing)1, StartTime, 64136, MathHelper.DegreesToRadians(0), MathHelper.DegreesToRadians(60));
+            triangulao.Rotate((OsbEasing)1, 65261, 65636, MathHelper.DegreesToRadians(60), MathHelper.DegreesToRadians(40));
+            triangulao.Rotate((OsbEasing)1, 65636, 66011, MathHelper.DegreesToRadians(40), MathHelper.DegreesToRadians(60));
+            triangulao.Rotate((OsbEasing)1, 66011, 66574, MathHelper.DegreesToRadians(60), MathHelper.DegreesToRadians(30));
+            triangulao.Rotate((OsbEasing)1, 66574, 67136, MathHelper.DegreesToRadians(30), MathHelper.DegreesToRadians(60));
+            triangulao.Rotate((OsbEasing)1, 67136, 68261, MathHelper.DegreesToRadians(60), MathHelper.DegreesToRadians(0));
+
+            triangulao.Rotate((OsbEasing)1, 68636, 68824, MathHelper.DegreesToRadians(0), MathHelper.DegreesToRadians(-10));
+            triangulao.Rotate((OsbEasing)1, 68824, 69011, MathHelper.DegreesToRadians(-10), MathHelper.DegreesToRadians(0));
+            triangulao.Rotate((OsbEasing)1, 69011, 70136, MathHelper.DegreesToRadians(0), MathHelper.DegreesToRadians(-60));
+
+            triangulao.Rotate((OsbEasing)1, 71636, 71824, MathHelper.DegreesToRadians(-60), MathHelper.DegreesToRadians(-50));
+            triangulao.Rotate((OsbEasing)1, 71824, 72011, MathHelper.DegreesToRadians(-50), MathHelper.DegreesToRadians(-60));
+            triangulao.Rotate((OsbEasing)1, 72011, 73511, MathHelper.DegreesToRadians(-60), MathHelper.DegreesToRadians(20));
+
+            triangulao.Scale((OsbEasing)1, StartTime, 64136, 1*1.2, 1.2*0.91);
+            triangulao.Scale((OsbEasing)1, 65261, 65636, 1.2*0.91, 1.2*0.88);
+            triangulao.Scale((OsbEasing)1, 65636, 66011, 1.2*0.88, 1.2*0.85);
+            triangulao.Scale((OsbEasing)1, 66011, 66574, 1.2*0.85, 1.2*0.805);
+            triangulao.Scale((OsbEasing)1, 66574, 67136, 1.2*0.805, 1.2*0.76);
+            triangulao.Scale((OsbEasing)1, 67136, 68261, 1.2*0.76, 1.2*0.65);
+
+            triangulao.Scale((OsbEasing)1, 68636, 68824, 1.2*0.65, 1.2*0.635);
+            triangulao.Scale((OsbEasing)1, 68824, 69011, 1.2*0.635, 1.2*0.62);
+            triangulao.Scale((OsbEasing)1, 69011, 70136, 1.2*0.62, 1.2*0.51);
+
+            triangulao.Scale((OsbEasing)1, 71636, 71824, 1.2*0.51, 1.2*0.495);
+            triangulao.Scale((OsbEasing)1, 71824, 72011, 1.2*0.495, 1.2*0.48);
+            triangulao.Scale((OsbEasing)1, 72011, 73511, 1.2*0.48, 1.2*0.37);
+
+            triangulao.Rotate((OsbEasing)4, 73886, 74261, MathHelper.DegreesToRadians(20), MathHelper.DegreesToRadians(0));
+            triangulao.Rotate((OsbEasing)4, 74261, 75011, MathHelper.DegreesToRadians(0), MathHelper.DegreesToRadians(360));
+
+            triangulao.Scale((OsbEasing)4, 73886, 74261, 0.37, 0.15);
+            triangulao.Scale((OsbEasing)4, 74261, 75011, 0.15, 1);
+
+            triangulao.Move((OsbEasing)4, 73886, 74261, new Vector2(320, 240), new Vector2(320, 200));
+            triangulao.Move((OsbEasing)4, 74261, 75011, new Vector2(320, 200), new Vector2(320, 420));
+
+            triangulao.Move((OsbEasing)4, 75011, 84011, new Vector2(320, 420), new Vector2(320, 130));
             
             flashBG.Additive(StartTime);
 
