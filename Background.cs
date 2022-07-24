00752e6a396fe47b3fd9bfc0f744270f8dc11f8b
Lucas Veiga <me@maot.dev>
2022-07-24 14:46:47 -0300

    Arrumando configurações

--- Background.cs
+++ Background.cs
@@ -1,5 +1,7 @@
 ﻿using StorybrewCommon.Scripting;
 using StorybrewCommon.Storyboarding;
+using OpenTK;
+using OpenTK.Graphics;
 using System.Linq;
 
 namespace StorybrewScripts
@@ -22,10 +24,13 @@ namespace StorybrewScripts
         {
             if (BackgroundPath == "") BackgroundPath = Beatmap.BackgroundPath ?? string.Empty;
             if (StartTime == EndTime) EndTime = (int)(Beatmap.HitObjects.LastOrDefault()?.EndTime ?? AudioDuration);
-
+            
             var bitmap = GetMapsetBitmap(BackgroundPath);
             var bg = GetLayer("").CreateSprite(BackgroundPath, OsbOrigin.Centre);
+            
+            
             bg.Scale(StartTime, 480.0f / bitmap.Height);
+            bg.Color(StartTime, Color4.Black);
             bg.Fade(StartTime, StartTime, 0, Opacity);
             bg.Fade(EndTime, EndTime + 500, Opacity, 0);
         }
